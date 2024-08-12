5d7f6f908db3138b9b1c8e6ddf732b02d133692f
shivam-devs <bdash625@gmail.com>
2024-08-12 10:16:03 +0530

    ggg

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ dotenv.config();
 app.use(
   cors({
     origin: ["*","http://localhost:3000"],
-    methods: ["GET", "POST", "DELETE"],
+    methods: ["GET", "POST", "DELETE","PATCH"],
     credentials: true,
   })
 );
@@ -61,9 +61,55 @@ app.post("/api/question", async (req, res) => {
     })
     .catch((err) => console.log(err));
 });
+app.patch("/api/question", async (req, res) => {
+  const {
+    id,
+    type,
+    incorrect_answers,
+    difficulty,
+    category,
+    question,
+    correct_answer,
+  } = req.body;
+  if( !mongoose.Types.ObjectId.isValid(id) )  return res.json({ msg: "Invalid id !" });
+  // validation
+  const duplicateCheck = await questions.findOne({_id:id});
+
+  if (! duplicateCheck)
+    return res.json({ msg: "question not exists" });
+
+  // add new train
+  const Question = await questions.findOne({_id:id});
+  if(Question){
+    questions.updateOne({_id:id},{$set:{type,
+      incorrect_answers,
+      difficulty,
+      category,
+      question,
+      correct_answer,}}).then((train) => {
+        return res.status(200).json({
+          'msg':'updated !'
+        });
+      })
+      .catch((err) => console.log(err));
+  }
+});
 app.get('/api/questions', async (req,res)=>{
   const outTrain = await questions.find().sort({ _id : -1});
   res.status(200).json(outTrain);
+})
+app.use(cors());
+app.delete('/api/question', async (req,res)=>{
+  const {
+    id,
+  } = req.body;
+  if( !mongoose.Types.ObjectId.isValid(id) )  return res.json({ msg: "Invalid id !" });
+  questions.deleteOne({_id:id}).then((train) => {
+    return res.status(200).json({
+      'msg':'deleted !'
+    });
+  })
+  .catch((err) => console.log(err));
 })
 app.use(cors());
 
